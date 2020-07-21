bcb788ae5bccf18851c8cdaefa8f03f6d5d2a58f
Milos Popovic <milos_popovic@outlook.com>
2020-07-21 18:26:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(name, num)
+    if roster.include?(num) == false #I don't understand this part
+       roster[num] = []
+    end
+      roster[num] << name
+  end
+  def grade(num)
+    roster[num]
+  end
+  def sort
+    roster.each do |num, name|
+      roster[num] = name.sort #sorting elements in an array, assosiated with certain key
+    end
+  end
+end
